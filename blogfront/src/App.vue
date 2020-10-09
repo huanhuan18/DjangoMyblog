657fdf5c17f38af32372b6dc9c3782ad0d860082
huanhuan18 <848368042@qq.com>
2020-10-10 01:31:34 +0800

    首页布局

--- blogfront/src/App.vue
+++ blogfront/src/App.vue
@@ -1,46 +1,67 @@
 <template>
   <div id="app">
-    <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949">
-    </el-switch>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- 头部导航 -->
+    <div id="top-menu" class="blogshader"></div>
+    <!-- 侧边栏 左 导航 -->
+    <div id="left-menu" class="blogshader"></div>
+    <!-- 页面内容 -->
+    <div id="content">
+      <el-row :gutter="20">
+        <el-col :span="8"
+          ><div class="blogshader" style="height: 250px"></div
+        ></el-col>
+        <el-col :span="8"
+          ><div class="blogshader" style="height: 250px"></div
+        ></el-col>
+        <el-col :span="8"
+          ><div class="blogshader" style="height: 250px"></div
+        ></el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8"
+          ><div class="blogshader" style="height: 250px"></div
+        ></el-col>
+        <el-col :span="8"
+          ><div class="blogshader" style="height: 250px"></div
+        ></el-col>
+        <el-col :span="8"
+          ><div class="blogshader" style="height: 250px"></div
+        ></el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8"
+          ><div class="blogshader" style="height: 250px"></div
+        ></el-col>
+        <el-col :span="8"
+          ><div class="blogshader" style="height: 250px"></div
+        ></el-col>
+        <el-col :span="8"
+          ><div class="blogshader" style="height: 250px"></div
+        ></el-col>
+      </el-row>
+
+      <!-- 页面底部 -->
+      <div id="footer" class="blogshader">
+        <span>Copyright@gf的个人博客</span>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
 
 <script>
-  export default {
-    data() {
-      return {
-        value: true
-      }
-    }
-  };
+export default {
+  data() {
+    return {
+      value: true,
+    };
+  },
+};
 </script>
 
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.el-col {
+  margin-top: 5px;
 }
 </style>
